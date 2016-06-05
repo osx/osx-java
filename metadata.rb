e81a18d53aacd5fbbab6d9f99dd7067dcd294e1d
Greg Levine <levine.greg@gmail.com>
2016-06-05 03:43:10 -0400

    The homebrew and java cookbooks must come first

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Installs the JDK on OS X using Homebrew.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'homebrew' # https://github.com/chef-cookbooks/homebrew/issues/70
+depends 'java'
+
 recipe 'osx-java::default', description
 
 supports 'mac_os_x'
